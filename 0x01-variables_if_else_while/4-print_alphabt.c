023a173e19a9dd755158f349b687044e03ef9fca
AlshimaaAhmed <alshimaa185@gmail.com>
2023-07-17 06:38:16 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,8 +6,12 @@
 int main(void)
 {
 char l = 'a';
-for (l = 'a' ; (l <= 'z') && (l != 'e') && (l != 'q') ; l++)
+for (l = 'a' ; l <= 'z' ; l++)
 {
+if ((l == 'e') || (l == 'q'))
+{
+continue;
+}
 putchar(l);
 }
 putchar('\n');
