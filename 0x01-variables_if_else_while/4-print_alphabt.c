2548b83a24bbe03a361a6de140df3d10d3f476a0
AlshimaaAhmed <alshimaa185@gmail.com>
2023-07-17 06:33:18 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - alphabet printing
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+char l = 'a';
+for (l = 'a' ; (l <= 'z') && (l != 'e') && (l != 'q') ; l++)
+{
+putchar(l);
+}
+putchar('\n');
+return (0);
+}
