d9ac20ce2436cee935aa5be06eab3a8bbfa240fd
AlshimaaAhmed <alshimaa185@gmail.com>
2023-07-20 08:34:40 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - print line
+ * @n:the number of under scorsthat create the line
+ * return: void
+ */
+void print_line(int n)
+{
+int i;
+for (i = 1 ; i <= n ; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
