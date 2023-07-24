e1bd43b454dd52d32efd295e90bfae639821dd74
AlshimaaAhmed <alshimaa185@gmail.com>
2023-07-24 11:13:04 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - culculate the length of the string
+ * @s: the pointer that will recifed the addriss of first element in the string
+ */
+int _strlen(char *s)
+{
+int len = 1;
+int i;
+for (i = 0 ; *(s + i) != '\0' ; i++)
+{
+	len += 1;
+}
+return len;
+}
