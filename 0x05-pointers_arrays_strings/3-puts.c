8f4bd1ca77528e52e5bd37f3a1025dde9ecc83d0
AlshimaaAhmed <alshimaa185@gmail.com>
2023-07-24 11:30:27 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - prints the string
+ * @str: pointer that reseve the string
+ * return: void
+ */
+void _puts(char *str)
+{
+int i;
+i = 0;
+while (*(str + i) != '\0')
+{
+_putchar(*(str + i));
+i++;
+}
+}
