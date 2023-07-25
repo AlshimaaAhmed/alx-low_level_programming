5b30227566f42125b016d1a47bfb4ca854e0035c
AlshimaaAhmed <alshimaa185@gmail.com>
2023-07-25 03:54:38 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - put every other characters
+ * @str: the pointer that will reseve the string
+ * Return: void
+ */
+void puts2(char *str)
+{
+int i;
+for (i = 0; *(str + i) != '\n' ; i++)
+{
+if (i % 2 == 0)
+{
+_putchar(str[i]);
+}
+}
+_putchar('\n');
+}
