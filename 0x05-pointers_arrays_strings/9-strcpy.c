0e23b6d54e6c3fa3ac52559efb82b40be2d38898
AlshimaaAhmed <alshimaa185@gmail.com>
2023-07-25 05:08:23 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * *_strcpy - copy string in another
+ * @dest: the final string
+ * @src: the coped string
+ * Return: dest copy string
+ */
+char *_strcpy(char *dest, char *src)
+{
+dest = src;
+return (dest);
+}
