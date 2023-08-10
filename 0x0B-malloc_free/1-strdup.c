3a512905ee0cda68dd481b0f5ec2693b1dedb35f
AlshimaaAhmed <alshimaa185@gmail.com>
2023-08-10 04:39:23 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+char *_strdup(char *str)
+{
+char *str2;
+unsigned int i;
+int size = 0;
+if (str == NULL)
+return (NULL);
+while(str)
+{
+size ++;
+}
+str2 = malloc(sizeof(char) * size)
+if (size == 0 || str2 == NULL)
+return (NULL);
+for (i = 0; i < size; i++)
+str2[i] = str[i];
+return (str2);
+}
