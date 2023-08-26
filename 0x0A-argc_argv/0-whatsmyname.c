fc60f9eda22e5c5edc8358600fd2642e59a58556
AlshimaaAhmed <alshimaa185@gmail.com>
2023-08-26 16:31:35 +0300

    new

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,8 @@
  * @argv:the array of arguments
  * Return:success always (0)
  */
-int main(int argc,  char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-printf("%s\n", argv[0]);
+printf("%s\n", *argv);
 return (0);
 }
