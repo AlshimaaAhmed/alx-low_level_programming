49ee7ccee8fe1df85e6d8e84e60ce11e9c1453a1
AlshimaaAhmed <alshimaa185@gmail.com>
2023-08-26 16:45:39 +0300

    task2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include "main.h"
+/**
+ * main -print all arguments
+ * @argc: the number of arguments
+ * @argv:the array of arguments
+ * Return:success always (0)
+ */
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+return (0);
+}
