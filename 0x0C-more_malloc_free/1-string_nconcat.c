e3f17610db670170c72536a1eaad8cef6f816731
AlshimaaAhmed <alshimaa185@gmail.com>
2023-10-09 10:59:14 -0700

    new

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,30 +11,35 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *ptr;
-unsigned int a = 0, i, j;
-if (s1 != '\0')
-a++;
+char *s;
+unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-ptr = (char *)malloc(a + n + 1);
-if (ptr == NULL)
+while (s1 && s1[len1])
+len1++;
+while (s2 && s2[len2])
+len2++;
+
+if (n < len2)
+s = malloc(sizeof(char) * (len1 + n + 1));
+else
+s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+if (!s)
 return (NULL);
 
-for (i = 0; s1 != '\0'; i++)
-ptr[i] = s1[i];
-
-for (j = 0; j < n; j++)
+while (i < len1)
 {
-if (s2 != '\0')
-{
-ptr[i] = s2[j];
-}
-else
-{
-break;
-}
+s[i] = s1[i];
 i++;
 }
-ptr[i] ='\0';
-return (ptr);
+
+while (n < len2 && i < (len1 + n))
+s[i++] = s2[j++];
+
+while (n >= len2 && i < (len1 + len2))
+s[i++] = s2[j++];
+
+s[i] = '\0';
+
+return (s);
 }
